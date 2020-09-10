11df70acf68d09621706e452043a122234e47f52
hanzhiqiang <1558249222han@gmail.com>
2020-09-10 18:06:52 +0800

    计算逆序对数中添加归并排序算法

--- nixu_pair.py
+++ nixu_pair.py
@@ -17,12 +17,41 @@ def reverse_pair(list_value):
     print(reverse_pair_count)
 
 
-l = [6, 2, 8, 1, 9, 0]
+#l = [6, 2, 8, 1, 9, 0]
 #    0  1  0  3  0  5
-reverse_pair(l)
-l = [1, 2, 3, 4, 5]
+#reverse_pair(l)
+#l = [1, 2, 3, 4, 5]
 #    0  1  0  3  0  5
-reverse_pair(l)
-l = [5, 4, 3, 2, 1]
+#reverse_pair(l)
+#l = [5, 4, 3, 2, 1]
 #    0  1  0  3  0  5
-reverse_pair(l)
+#reverse_pair(l)
+
+def merge_two_list(left_nums, right_nums):
+    arr = []
+    while i < len(left_nums) and j < len(right_nums):
+        if left_nums[i] <= right_nums[j]:
+            arr.append(left_nums[i])
+            i+=1
+        else:
+            arr.append(right_nums[j])
+            j+=1
+
+    arr.extend(left_nums[i:]) 
+    arr.extend(right_nums[j:]) 
+    return arr
+
+def merge(nums):
+    length = len(nums)
+    if length <= 1:
+        return nums
+
+    mid = length // 2
+    left_nums = merge(nums[:mid])
+    right_nums = merge(nums[mid:])
+
+    return merge_two_list(left_nums, right_nums)
+
+l = [6, 3, 9, 1, 8, 0, 7]
+
+print(merge(l))
