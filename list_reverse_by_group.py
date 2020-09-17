7a5b2f8a57d67c2a0ff369ae4b69494e1710b9e9
hanzhiqiang <1558249222han@gmail.com>
2020-09-17 16:13:00 +0800

    refactore folder struct

--- list_reverse_by_group.py
+++ list_reverse_by_group.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from self_link import LinkList
+from lib.self_link import LinkList, Node
 
 # reverse
 l = LinkList()
@@ -10,4 +10,20 @@ l.add(2)
 l.add(3)
 l.add(4)
 l.add(5)
+l.print_link_list()
+
+
+# reverse
+def reverse():
+    head = l.head
+    phead = head 
+    while phead and phead.next:
+        a = phead.next
+        b = phead.next.next
+        a.next = b.next
+        b.next = a
+        phead = a
+    return head
+
+reverse()
 l.print_link_list()
