1680773533f8d53c48dd6f31b9457d3d57a47a59
tars <1558249222han@gmail.com>
2020-11-01 16:48:58 +0800

    Create tree_k_small.py

---
+++ tree_k_small.py
@@ -0,0 +1,60 @@
+给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
+
+说明：
+你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。
+
+示例 1:
+
+输入: root = [3,1,4,null,2], k = 1
+   3
+  / \
+ 1   4
+  \
+   2
+输出: 1
+示例 2:
+
+输入: root = [5,3,6,2,4,null,null,1], k = 3
+       5
+      / \
+     3   6
+    / \
+   2   4
+  /
+ 1
+输出: 3
+进阶：
+如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化 kthSmallest 函数？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+# tips，中序遍历二叉树是从小到大排列的。
+#
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        self.dfs_count = 0
+        self.dfs_value = 0
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            self.dfs_count += 1
+            if self.dfs_count == k:
+                self.dfs_value = root.val
+                return
+            dfs(root.right)
+        dfs(root)
+        return self.dfs_value
