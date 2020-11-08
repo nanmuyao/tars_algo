31efec566e2bd5df3468cb75e886542b893d8872
nanmuyao <1558249222han@gmail.com>
2020-11-08 16:25:40 +0800

    堆排序

--- heap_property_dequeue.py
+++ heap_property_dequeue.py
@@ -2,7 +2,7 @@
 
 class Heap:
     def __init__(self):
-        self.stack = [90,80,70,60,40,30,20,10,50]
+        self.stack = [90,85,70,60,80,30,20,10,50,40]
 
     def insert(self, num):
         self.stack.append(num)
@@ -37,37 +37,66 @@ class Heap:
         root_index = 0
         length = len(self.stack) - 1
         stack = self.stack
-        while True:
+        while root_index < length:
             left_index = 2 * root_index + 1
             right_index = 2 * root_index + 2
-
-            if left_index > length and right_index > length:
-                # 如果索引越界那么结束
+            max = left_index
+            if left_index > length:
                 break
-            if left_index <= length and right_index > length:
-               if self.stack[left_index] > self.stack[root_index]:
-                   self.stack[left_index], self.stack[root_index] = \
-                     self.stack[root_index], self.stack[left_index]
-                   root_index = left_index
+            if right_index <= length and stack[left_index] < stack[right_index]:
+                max = right_index
+            
+            if stack[root_index] < stack[max]:
+                stack[root_index], stack[max] = stack[max], stack[root_index]
+                root_index = max
+            else:
+                break
+            
+                        
+# heap_sort 代码实现
+def build(arr, root, end):
+    while True:
+        left = 2 * root + 1 # 左子节点的位置
+        right =  2 * root + 2
+        max = left
+        if left > end: # 若左子节点超过了最后一个节点，则终止循环
+            # 这里很秒，只有这个写法很简洁
+            break
+        if (right <= end) and (arr[right] > arr[left]): 
+            # 若右子节点在最后一个节点之前，并且右子节点比左子节点大，则我们的孩子指针移到右子节点上
+            max = right
+        if arr[max] > arr[root]: 
+            # 若最大的孩子节点大于根节点，则交换两者顺序，并且将根节点指针，移到这个孩子节点上
+            arr[max], arr[root] = arr[root], arr[max]
+            root = max
+        else:
+            break
 
-            if left_index > length and right_index <= length:
-               if self.stack[right_index] > self.stack[root_index]:
-                   self.stack[right_index], self.stack[root_index] = \
-                     self.stack[root_index], self.stack[right_index]
-                   root_index = right_index
-            if left_index <= length and right_index <= length:
-               max_value = max(stack[root_index], stack[left_index], stack[right_index])
-               if max_value == stack[root_index]:
-                   break
-               elif max_value == stack[left_index]:
-                   stack[left_index], stack[root_index] = stack[root_index], stack[left_index]
-                   root_index = left_index
-               elif max_value == stack[right_index]:
-                   stack[right_index], stack[root_index] = stack[root_index], stack[right_index]
-               else:
-                   break
-
+def heap_sort(arr):
+    print('build before', arr)
+    n = len(arr)
+    # 这里是第一个非堆节点
+    first_root = n // 2 - 1 
+    # 确认最深最后的那个根节点的位置
+    for root in range(first_root, -1, -1): 
+        # 由后向前遍历所有的根节点，建堆并进行调整
+        build(arr, root, n - 1)
+        
+    print('build after', arr)
+    for end in range(n - 1, 0, -1): 
+        # 调整完成后，将堆顶的根节点与堆内最后一个元素调换位置，此时为数组中最大的元素，
+        # 然后重新调整堆，将最大的元素冒到堆顶。依次重复上述操作
+        arr[0], arr[end] = arr[end], arr[0]
+        build(arr, 0, end - 1)
+        
 
 if __name__ == "__main__":
-    h = Heap()
-    h.insert(85)
+    # h = Heap()
+    # h.insert(85)
+    # print(h.stack)
+    # print(h.d())
+    # print(h.stack)
+
+    arr = [5, 8, 1, 0, 3, 7, 6, 2, 9]
+    heap_sort(arr)
+    print(arr)
--- heap_property_dequeue_2.py
+++ heap_property_dequeue_2.py
@@ -1,12 +1,13 @@
+# encoding='utf-8'
+
 class MaxHeap:
-    heap = [90,80,70,60,40,30,20,10,50]
- 
+    heap = []
+
     @staticmethod
     def insert(num):
         MaxHeap.heap.append(num)
         MaxHeap.shift_up()
-        print(MaxHeap.heap)
-        
+
     @staticmethod
     def shift_up():
         current_id = len(MaxHeap.heap) - 1
@@ -15,11 +16,91 @@ class MaxHeap:
             if MaxHeap.heap[parent_id] >= MaxHeap.heap[current_id]:
                 break
             else:
-                MaxHeap.heap[parent_id], MaxHeap.heap[current_id] = \
-                    MaxHeap.heap[current_id], MaxHeap.heap[parent_id]
+                MaxHeap.heap[parent_id], MaxHeap.heap[current_id] = MaxHeap.heap[current_id], MaxHeap.heap[parent_id]
                 current_id = parent_id
-                parent_id = (current_id -1)//2
-                
+                parent_id = (current_id - 1)//2
+
+    @staticmethod
+    def delate(num):
+        if num == MaxHeap.heap[-1]:
+            MaxHeap.heap.pop() 
+            return
+        temp = MaxHeap.heap.pop()
+
+        ind = MaxHeap.heap.index(num)
+        MaxHeap.heap[ind] = temp
+        MaxHeap.shift_down(ind)
+
+    @staticmethod
+    def shift_down(ind):
+        current_id = ind
+        child_id_left = current_id * 2 + 1
+        child_id_right = current_id * 2 + 2
+        while current_id < len(MaxHeap.heap) - 1:
+            # �����ǰ�ڵ�ΪҶ�ӽڵ㣬shift_down���
+            if current_id * 2 + 1 > len(MaxHeap.heap) - 1:
+                break
+            # ????????????????????????
+            if current_id * 2 + 1 == len(MaxHeap.heap) - 1:
+                if MaxHeap.heap[current_id] > MaxHeap.heap[-1]:
+                    break
+                else:
+                    MaxHeap.heap[current_id], MaxHeap.heap[-1] = MaxHeap.heap[-1], MaxHeap.heap[current_id]
+                    break
+            # ?????????????????????????
+            if MaxHeap.heap[current_id] > max(MaxHeap.heap[child_id_left], MaxHeap.heap[child_id_right]):
+                break
+            else:
+                if MaxHeap.heap[child_id_right] > MaxHeap.heap[child_id_left]:
+                    MaxHeap.heap[child_id_right], MaxHeap.heap[current_id] = MaxHeap.heap[current_id], MaxHeap.heap[child_id_right]
+                    current_id = child_id_right
+                    child_id_left = current_id * 2 + 1
+                    child_id_right = current_id * 2 + 2
+                else:
+                    MaxHeap.heap[child_id_left], MaxHeap.heap[current_id] = MaxHeap.heap[current_id], MaxHeap.heap[child_id_left]
+                    current_id = child_id_left
+                    child_id_left = current_id * 2 + 1
+                    child_id_right = current_id * 2 + 2
+
+    @staticmethod
+    def extract_max():
+        num = MaxHeap.heap[0]
+        try:
+            MaxHeap.delate(num)
+            return num
+        except:
+            return num
+
+    @staticmethod
+    def heap_sort(arr):
+        for n in arr:
+            MaxHeap.insert(n)
+        for i in range(len(arr)):
+            arr[i] = MaxHeap.extract_max()
+
+    @staticmethod
+    def heapify(arr):
+        MaxHeap.heap = arr
+        n = (len(arr) - 1)//2
+        while n >= 0:
+            MaxHeap.shift_down(n)
+            n -= 1
+
+    @staticmethod
+    def heap_sort2(arr):
+        MaxHeap.heapify(arr)
+        res = []
+        for i in range(len(arr)):
+            res.append(MaxHeap.extract_max())
+        return res
+
+
 if __name__ == "__main__":
     h = MaxHeap()
-    h.insert(85)
+    arr = [90,85,70,60,80,30,20,10,50,40]
+    MaxHeap.heap_sort(arr)
+    print(arr)
+    
+    arr = [90,85,70,60,80,30,20,10,50,40]
+    arr = MaxHeap.heap_sort2(arr)
+    print(arr)
