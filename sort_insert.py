bf74d91805256156e30a2dd88b139ab459e96aac
nanmuyao <1558249222han@gmail.com>
2020-11-21 11:46:36 +0800

    插入排序

--- sort_insert.py
+++ sort_insert.py
@@ -1,5 +1,8 @@
+arr = [2, 1, 7, 9, 5, 8]
+print('sort before', arr)
+
 # https://www.runoob.com/python3/python-insertion-sort.html
-def sort_insert(arr):
+def sort_insert1(arr):
     length = len(arr)
     for i in range(1, length):
         cursor_value = arr[i]
@@ -11,7 +14,22 @@ def sort_insert(arr):
         arr[j + 1] = cursor_value
     return arr
 
-arr = [2, 1, 7, 9, 5, 8]
-print('sort before', arr)
-arr = sort_insert(arr)
-print('sort after', arr)
+
+arr = sort_insert1(arr)
+print('1 sort after', arr)
+
+
+# times 2 写法简洁了一些，但是空间复杂度变大了，每一次交换数组
+def sort_insert2(arr):
+    length = len(arr)
+    for i in range(1, length):
+        cursor = i
+        while i >= 1:
+            i-=1
+            if arr[cursor] < arr[i]:
+                arr[cursor], arr[i] = arr[i], arr[cursor]
+                cursor = i
+    return arr
+
+arr = sort_insert2(arr)
+print('2 sort after', arr)
