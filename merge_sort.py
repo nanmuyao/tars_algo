bc8ef519ea10e6acd6c0c8f324b3364dbc2e6df7
nanmuyao <1558249222han@gmail.com>
2020-11-21 11:14:47 +0800

    第n次联系归并排序

--- merge_sort.py
+++ merge_sort.py
@@ -1,27 +1,57 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-def merge_two_list(left, right):
-    list_merged = []
-    i = 0
-    j = 0
+
+l = [1, 3, 2, 4, 0]
+
+# def merge_two_list(left, right):
+#     list_merged = []
+#     i = 0
+#     j = 0
+#     while i < len(left) and j < len(right):
+#         if left[i] < right[j]:
+#             list_merged.append(left[i])
+#             i+=1
+#         else:
+#             list_merged.append(right[j])
+#             j+=1
+#     list_merged.extend(left[i:])
+#     list_merged.extend(right[j:])
+#     return list_merged
+    
+# def merge(arr):
+#     if len(arr) <= 1:
+#         return arr
+#     mid = len(arr)//2
+#     left = merge(arr[:mid])
+#     right = merge(arr[mid:])
+#     return merge_two_list(left, right)
+    # 
+
+# print(merge(l))
+
+
+# time n
+def sort(left, right):
+    i, j = 0, 0
+    arr = []
     while i < len(left) and j < len(right):
         if left[i] < right[j]:
-            list_merged.append(left[i])
+            arr.append(left[i])
             i+=1
         else:
-            list_merged.append(right[j])
+            arr.append(right[j])
             j+=1
-    list_merged.extend(left[i:])
-    list_merged.extend(right[j:])
-    return list_merged
-    
+    arr.extend(left[i:])
+    arr.extend(right[j:])
+    return arr
+
 def merge(arr):
     if len(arr) <= 1:
         return arr
-    mid = len(arr)//2
+    mid = len(arr) // 2
     left = merge(arr[:mid])
     right = merge(arr[mid:])
-    return merge_two_list(left, right)
-    
-l = [1, 3, 2, 4, 0]
+    arr = sort(left, right)
+    return arr
+
 print(merge(l))
