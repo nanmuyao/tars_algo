c032943f2551e83f1dec3f3d8f368ec6391d62a3
tars <1558249222han@gmail.com>
2020-12-18 22:19:20 +0800

    Create mypow_50.py

---
+++ leetcode/mypow_50.py
@@ -0,0 +1,48 @@
+class Solution(object):
+    d = {}
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if not n:
+            return 1
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+        if n % 2:
+            return x * self.myPow(x, n-1)
+        Solution.d[n] = self.myPow(x, n/2)
+        return Solution.d[n] * Solution.d[n]
+        
+class Solution(object):
+    d = {}
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if not n:
+            return 1
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+        if n % 2:
+            return x * self.myPow(x, n-1)
+        return self.myPow(x, n/2) * self.myPow(x, n/2)
+
+class Solution(object):
+    d = {}
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if not n:
+            return 1
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+        if n % 2:
+            return x * self.myPow(x, n-1)
+        return self.myPow(x*x, n/2) 
