8388bcdb97e8e91e9f63493c0071d34415305e11
tars <1558249222han@gmail.com>
2021-02-16 21:33:28 +0800

    Create 206_reverse_link_list.py

---
+++ leetcode/206_reverse_link_list.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        pre = None
+        cur = head
+        while cur:
+            cur.next, pre, cur = pre, cur, cur.next
+        return pre
