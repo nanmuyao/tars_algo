6ca0fccf4e9f48535d1e898db3aa1d36036590e3
tars <1558249222han@gmail.com>
2021-05-03 08:58:53 +0800

    Update 239_sliding-window-maximum.py

--- leetcode/239_sliding-window-maximum.py
+++ leetcode/239_sliding-window-maximum.py
@@ -15,7 +15,8 @@ class Solution(object):
             # 最左侧的元素根据坐标可以判断是否有效
             if i >= k and win[0] <= i - k:
                 win.pop(0)
-            # 最右侧元素根据value判断，如果窗口中的元素小于要加入的元素，那么弹出
+            # 最右侧元素根据value判断，如果窗口中的元素小于要加入的元素，
+            # 那么弹出窗口中的元素，新元素准备入队
             while win and nums[win[-1]] < v:
                 win.pop()
 
