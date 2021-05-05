eb44a607919f28ee17e89fabe2296908aab1f821
nanmuyao <1558249222@qq.com>
2021-05-05 09:47:52 +0800

    add tip

--- leetcode/50_powx-n.py
+++ leetcode/50_powx-n.py
@@ -18,7 +18,7 @@ class Solution(object):
             return 1 / self.myPow(x, -n)
 
         if n % 2 == 0:
-            # 这里很骚气
+            # 这里很骚气， 结果在栈的最底层传递上来， 问题规模指数级别的缩小，x* x 在一直计算
             return self.myPow(x * x, n/2)
         else:
             return x * self.myPow(x, n - 1)
