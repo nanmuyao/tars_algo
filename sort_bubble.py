c6ae16f87674e314f37e74be93271a1367c01412
tars <1558249222han@gmail.com>
2021-08-31 23:28:53 +0800

    Update sort_bubble.py

--- sort_bubble.py
+++ sort_bubble.py
@@ -13,3 +13,11 @@ print('sort before', arr)
 arr = sort_bubble(arr)
 print('sort after', arr)
 
+def sort_bubble_v2():
+    l = [1, 3, 5, 2, 4, 6]
+    n = len(l)
+    for i, vi in enumerate(l):
+        for j, vj in enumerate(l):
+            if vi > vj:
+                l[i], l[j] = l[j], l[i]
+    print(l)
